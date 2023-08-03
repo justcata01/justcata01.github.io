faecc955d5e14f377f18d4bd26c1d5d198057435
cata <102311027+justcata01@users.noreply.github.com>
2023-08-03 23:56:44 +0300

    Added Animation

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-function redirect(name){
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('shown')
+        }
+        else {
+            entry.target.classList.remove('shown')
+        }
+    })
+}, {threshold: 0.75});
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+function redirect(name) {
     var url = "";
-    switch(name){
+    switch (name) {
         case "linkedin": url = "https://www.linkedin.com/in/catalin-brezoi/"; break;
         case "github": url = "https://github.com/justcata01"; break;
+        case "highschool": url = "https://www.ctenergetic.ro/"; break;
+        case "uni": url = "https://www.anmb.ro/"; break;
     }
     window.location = url;
 }
